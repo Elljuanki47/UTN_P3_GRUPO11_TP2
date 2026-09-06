5ab2b3208243d1be29733e156bf3eb2c463f7a00
DeleBayeriano <deledicquejoaquin@gmail.com>
2026-09-06 15:03:53 -0300

    ej3 mod v5

--- TP2_GRUPO_11/Ejercicio3.aspx.cs
+++ TP2_GRUPO_11/Ejercicio3.aspx.cs
@@ -18,14 +18,23 @@ namespace TP2_GRUPO_11
         protected void lnkRojo_Click(object sender, EventArgs e)
         {
             lblTexto.ForeColor = System.Drawing.Color.Red;
+            lnkRojo.ForeColor = System.Drawing.Color.Red;
+            lnkAzul.ForeColor = System.Drawing.Color.Black;
+            lnkVerde.ForeColor = System.Drawing.Color.Black;
         }
         protected void lnkAzul_Click(object sender, EventArgs e)
         {
             lblTexto.ForeColor = System.Drawing.Color.Blue;
+            lnkRojo.ForeColor = System.Drawing.Color.Black;
+            lnkAzul.ForeColor = System.Drawing.Color.Blue;
+            lnkVerde.ForeColor = System.Drawing.Color.Black;
         }
         protected void lnkVerde_Click(object sender, EventArgs e)
         {
             lblTexto.ForeColor = System.Drawing.Color.Green;
+            lnkRojo.ForeColor = System.Drawing.Color.Black;
+            lnkAzul.ForeColor = System.Drawing.Color.Black;
+            lnkVerde.ForeColor = System.Drawing.Color.Green;
         }
     }
 }
